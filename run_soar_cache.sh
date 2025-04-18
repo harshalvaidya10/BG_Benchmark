f7540e47ab5dca81aa37b9effa9931d0034f4782
Cassia <40779641+FredericaErath@users.noreply.github.com>
2025-04-17 22:19:08 -0700

    change scripts

--- run_soar_cache.sh
+++ run_soar_cache.sh
@@ -2,26 +2,27 @@
 
 # Define array of configurations (populateWorkload, doCache, directory)
 CONFIGS=(
-  "workloads/populateDB_1000 true exp_soar_cache_1000_10"
-  "workloads/populateDB_1000_2 true exp_soar_cache_1000_100"
-  "workloads/populateDB_10000 true exp_soar_cache_10000_10"
-  "workloads/populateDB_10000_2 true exp_soar_cache_10000_100"
-  "workloads/populateDB_1000 false exp_soar_withoutcache_1000_10"
-  "workloads/populateDB_1000_2 false exp_soar_withoutcache_1000_100"
-  "workloads/populateDB_10000 false exp_soar_withoutcache_10000_10"
-  "workloads/populateDB_10000_2 false exp_soar_withoutcache_10000_100"
+  "workloads/populateDB_1000 true exp_soar_cache_1000_10 workloads/ListFriendsAction"
+  "workloads/populateDB_1000_2 true exp_soar_cache_1000_100 workloads/ListFriendsAction"
+  "workloads/populateDB_10000 true exp_soar_cache_10000_10 workloads/ListFriendsAction2"
+  "workloads/populateDB_10000_2 true exp_soar_cache_10000_100 workloads/ListFriendsAction2"
+  "workloads/populateDB_1000 false exp_soar_withoutcache_1000_10 workloads/ListFriendsAction"
+  "workloads/populateDB_1000_2 false exp_soar_withoutcache_1000_100 workloads/ListFriendsAction"
+  "workloads/populateDB_10000 false exp_soar_withoutcache_10000_10 workloads/ListFriendsAction2"
+  "workloads/populateDB_10000_2 false exp_soar_withoutcache_10000_100 workloads/ListFriendsAction2"
 )
 
 for CONFIG in "${CONFIGS[@]}"; do
   # Split string into parts
-  read -r POPULATE_WORKLOAD DOCACHE DIRECTORY <<< "$CONFIG"
+  read -r POPULATE_WORKLOAD DOCACHE DIRECTORY WORKLOAD<<< "$CONFIG"
 
-  echo "========== Running: $POPULATE_WORKLOAD | DOCACHE=$DOCACHE | DIRECTORY=$DIRECTORY =========="
+  echo "========== Running: $POPULATE_WORKLOAD | DOCACHE=$DOCACHE | DIRECTORY=$DIRECTORY | WORKLOAD=$WORKLOAD =========="
 
   ./rating_pipeline.sh \
     -populateWorkload "$POPULATE_WORKLOAD" \
     -doCache "$DOCACHE" \
-    -directory "$DIRECTORY"
+    -directory "$DIRECTORY" \
+    -workload "$WORKLOAD"
 
   echo "========== Finished: $DIRECTORY =========="
   echo ""
